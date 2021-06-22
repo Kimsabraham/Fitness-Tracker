e050d58fad7cf3f398579d3dda5873f9a53842a6
Abraham Kim <krnpride730@yahoo.com>
2021-06-22 19:03:50 -0400

    deleted routes folder and added routes to server

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
 
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,12 +18,59 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
-// routes
-app.use(require("./routes/api.js"));
+/// Html routes
+
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+/// API routes
+
+app.get("/api/workouts", (req, res) => {
+  db.Workout.aggregate([
+    { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+  ]).then((data) => {
+    res.json(data);
+  });
+});
+
+app.put("/api/workouts/:id", (req, res) => {
+  db.Workout.findByIdAndUpdate(
+    req.params.id,
+    { $push: { exercises: req.body } },
+    { new: true, runValidators: true }
+  )
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.json(err));
+});
+
+app.post("/api/workouts", (req, res) => {
+  db.Workout.create({}).then((data) => {
+    res.json(data);
+  });
+});
+
+app.get("/api/workouts/range", (req, res) => {
+  db.Workout.aggregate([
+    { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+  ])
+    .sort({ day: -1 })
+    .limit(10)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.json(err));
+});
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`Were listening ${PORT}!`);
 });
